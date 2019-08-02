f0c5a52820a15411389d507a9d5c02eef57b3d91
Kevin Choi <kevin.choi@yale.edu>
2019-08-02 02:34:32 -0400

    Debug logs

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ io.on('connection', socket => {
         //     msg = await translate(msg, guestLangs[socket.id]);
         // }
         msg = await translate(msg, 'ru');
-        console.log(msg);
+        console.log(msg, msg[0], msg[1].data.translations);
 		io.to(currRoom).emit('newMsg', msg);
 	});
 
